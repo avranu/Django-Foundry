65cf2d6fe9e744cd84ee597d8f3d362d2c3b1679
Avranu <jess.a.mann@gmail.com>
2023-06-30 18:38:43 -0400

    update project structure

--- src/djangofoundry/scripts/app/app.py
+++ src/djangofoundry/scripts/app/app.py
@@ -40,37 +40,10 @@ from jinja2 import Environment, FileSystemLoader
 from djangofoundry.scripts.utils.exceptions import DbStartError, UnsupportedCommandError
 from djangofoundry.scripts.utils.settings import Settings
 from djangofoundry.scripts.db import Db
+from djangofoundry.scripts.app.actions import Actions
 
 logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
 logger = logging.getLogger(__name__)
-
-class Actions(Enum):
-	"""
-	Defines the commands that can be used to interact with django.
-
-	Attributes:
-		start:
-			start the django server
-		test:
-			runs our unit/integration tests
-	"""
-	START = "runserver"
-	TEST = "test"
-	STOP = "stop"
-	RESTART = "restart"
-	STATUS = "status"
-	SETUP = 'setup'
-	PAGE = 'page'
-	MODEL = 'model'
-
-	def __str__(self):
-		"""
-		Turns an option into a string representation
-		"""
-		return self.value
-
-	def __repr__(self):
-		return self.value
 
 class App:
 	"""
@@ -101,6 +74,130 @@ class App:
 			raise ValueError('Command has not been set yet')
 		return self._command
 
+	def get_argument(self, argument_name : str, args : tuple, kwargs : dict ) -> Any:
+		"""
+		Retrieves an argument from args/kwargs.
+
+		This is useful for methods like self.perform() where we want to pass arguments to an arbitrary method, which may be different per command.
+
+		Args:
+			argument_name (str):
+				The name of the argument to retrieve
+			args (tuple):
+				The list of arguments passed to the method
+			kwargs (dict):
+				The dictionary of keyword arguments passed to the method
+
+		Returns:
+			The argument value, if it exists. Otherwise, None.
+
+		Examples:
+			>>> class Foo(App):
+			>>> 	def change_page(self, *args, **kwargs):
+			>>> 		argument = self.get_argument('page_name', args, kwargs)
+			>>> 		print('page_name = ' + argument)
+			>>> foo = Foo()
+			>>> foo.change_page('home')
+			page_name = home
+		"""
+		if len(args) == 1:
+			return args[0]
+		else:
+			return kwargs.get(argument_name, None)
+		
+	def create_venv(self, venv_name : str = 'venv') -> bool:
+		"""
+		Create a virtual environment for our project and enter it.
+		"""
+		logger.info('Creating virtual environment...')
+		try:
+			subprocess.check_output(['python', '-m', 'venv', 'venv'], stderr=subprocess.STDOUT)
+		except subprocess.CalledProcessError as e:
+			logger.error(f'Could not create virtual environment: {e.output}')
+			return False
+
+		logger.info('Activating virtual environment...')
+		try:
+			# If windows...
+			if platform.system() == 'Windows' or platform.system() == 'win32':
+				activate_command = os.path.join(venv_name, 'Scripts', 'activate')
+			else:
+				activate_command = os.path.join(venv_name, 'bin', 'activate')
+
+			subprocess.check_call([f'. {activate_command}'], shell=True)
+		except subprocess.CalledProcessError as e:
+			logger.error(f'Could not activate virtual environment: {e.output}')
+			return False
+
+		return True
+		
+	def pip_install(self, package_name : str) -> bool:
+		"""
+		Install a python package using pip, and add it (with version) to requirements.txt. 
+
+		Args:
+			pip_package (str): The name of the package(s) to install, separated by a space
+
+		Returns:
+			bool: True if the package was installed successfully, False otherwise.
+
+		Examples:
+			>>> app = App()
+			>>> app.pip_install('requests')
+			True
+
+			>>> app.pip_install('requests==2.26.0')
+			True
+		"""
+		# If the package_name contains multiple packages, split them and install them individually
+		if ' ' in package_name:
+			for single_package_name in package_name.split(' '):
+				if not self.pip_install(single_package_name):
+					return False
+			return True
+		
+		# If requirements.txt doesn't exist, create it
+		if not os.path.exists('requirements.txt'):
+			logger.info('Creating requirements.txt...')
+			with open('requirements.txt', 'w') as f:
+				f.write('')
+
+		# Check if the package is already in requirements.txt
+		# TODO: this ignores version numbers (therefore not handling updates)
+		with open('requirements.txt', 'r') as f:
+			if package_name in f.read():
+				logger.info(f'{package_name} already in requirements.txt. Skipping pip install.')
+				return True
+
+		# Install the package, capture output so that we can determine the version number of the package
+		logger.info(f'Installing {package_name}...')
+		install_output = subprocess.check_output([sys.executable, '-m', 'pip', 'install', package_name], stderr=subprocess.STDOUT).decode('utf-8')
+
+		# Grab the version number from the output
+		output = re.search(r'Successfully installed (.*)', install_output)
+
+		if not output:
+			logger.warning(f'Could not find a version number after installing {package_name}. Output: {install_output}')
+			return False
+		
+		version = output.group(1).split('-')[-1]
+
+		if version is None:
+			logger.warning(f'Could not determine version number for {package_name}')
+			return False
+		
+		# Ensure version is a valid version number
+		if not re.match(r'^\d+\.\d+\.\d+$', version):
+			logger.warning(f'Version number for {package_name} is not valid: {version}')
+			return False
+
+		# Add package (and version #) to requirements.txt. 
+		logger.info(f'Adding {package_name} to requirements.txt...')
+		with open('requirements.txt', 'a') as f:
+			f.write(f'{package_name}>={version}\n')
+
+		return True
+
 	def django_setup(self) -> str:
 		"""
 		Setup the Django project and app with given names.
@@ -110,13 +207,53 @@ class App:
 		Returns:
 			A string indicating the status of the setup.
 		"""
-		os.makedirs(self.backend_dir, exist_ok=True)
-		os.chdir(self.backend_dir) # Switch to the backend directory before running Django commands
-		self.run_subprocess(["pip", "install", "django"])
-		self.run_subprocess(["django-admin", "startproject", self.project_name, '.'])
-		self.run_subprocess(["python", "manage.py", "startapp", self.project_name])
-		os.chdir(self.directory) # Switch back to the original directory
+		try:
+			os.makedirs(self.backend_dir, exist_ok=True)
+			# Switch to the backend directory before running Django commands
+			os.chdir(self.backend_dir) 
+			self.run_subprocess(["pip", "install", "django"])
+			self.run_subprocess(["django-admin", "startproject", self.project_name, '.'])
+			self.run_subprocess(["python", "manage.py", "startapp", self.project_name])
+		finally:
+			# Switch back to the original directory
+			os.chdir(self.directory) 
+
 		return f"Django setup completed for {self.project_name}"
+	
+	def setup_frontend(self) -> str:
+		"""
+		Setup the frontend (including package.json) for an arbitrary frontend framework.
+	
+		Returns:
+			A string indicating the status of the setup.
+		"""
+		try:
+			os.makedirs(self.frontend_dir, exist_ok=True)
+			# Switch to the frontend directory before installing npm packages
+			os.chdir(self.frontend_dir) 
+			self.run_subprocess(["npm", "init", "-y"])
+
+			with open('package.json', encoding="utf-8") as f:
+				data = json.load(f)
+
+			# Modify the existing package.json file to include our params
+			data['name'] = self.project_name
+			data['version'] = '1.0.0'
+			data['description'] = f'{self.project_name} - an Angular-Django project'
+			data['main'] = 'index.js'
+			data['scripts'] = { 'test': 'echo "Error: no test specified" && exit 1' }
+			data['author'] = getpass.getuser()
+			data['license'] = 'BSD-3-Clause'
+
+			with open('package.json', 'w', encoding="utf-8") as f:
+				json.dump(data, f, indent=2)
+
+			# TODO 3 sparate npm commands can likely be consolidated into 2 or 1.
+			self.run_subprocess(["npm", "install"])
+		finally:
+			os.chdir(self.directory) # Switch back to the original directory
+
+		return f"Frontend setup completed for {self.project_name}"
 
 	def angular_setup(self) -> str:
 		"""
@@ -125,30 +262,163 @@ class App:
 		Returns:
 			A string indicating the status of the setup.
 		"""
-		os.makedirs(self.frontend_dir, exist_ok=True)
-		os.chdir(self.frontend_dir) # Switch to the frontend directory before running Angular commands
-		self.run_subprocess(["npm", "init", "-y"])
+		self.setup_frontend()
 
-		with open('package.json', encoding="utf-8") as f:
-			data = json.load(f)
+		try:
+			os.chdir(self.frontend_dir) 
 
-		data['name'] = self.project_name
-		data['version'] = '1.0.0'
-		data['description'] = f'{self.project_name} - an Angular-Django project'
-		data['main'] = 'index.js'
-		data['scripts'] = { 'test': 'echo "Error: no test specified" && exit 1' }
-		data['author'] = getpass.getuser()
-		data['license'] = 'BSD-3-Clause'
+			# TODO 3 sparate npm commands can likely be consolidated into 2 or 1.
+			self.run_subprocess(["npm", "install"])
+			self.run_subprocess(["npm", "install", "@angular/cli"])
+			self.run_subprocess(["ng", "new", self.project_name, "--skip-git", "--skip-install"])
+		finally:
+			# Switch back to the original directory
+			os.chdir(self.directory) 
 
-		with open('package.json', 'w', encoding="utf-8") as f:
-			json.dump(data, f, indent=2)
-
-		# TODO 3 sparate npm commands can likely be consolidated into 2 or 1.
-		self.run_subprocess(["npm", "install"])
-		self.run_subprocess(["npm", "install", "@angular/cli"])
-		self.run_subprocess(["ng", "new", self.project_name, "--skip-git", "--skip-install"])
-		os.chdir(self.directory) # Switch back to the original directory
 		return f"Angular setup completed for {self.project_name}"
+	
+	def append_django_apps(self, app_name: str) -> bool:
+		"""
+		Add the app to the INSTALLED_APPS list in the settings/base.py file
+		"""
+		settings_file = f'{self.backend_dir}/{self.project_name}/settings/base.py'
+
+		# Load the entire settings/base.py file into memory
+		# TODO try catch
+		with open(settings_file, 'r') as f:
+			contents = f.read()
+
+			# Get the installed apps list
+			codeblock = re.search(r'INSTALLED_APPS = \[(.*)\]', contents, re.DOTALL)
+			if not codeblock:
+				# TODO better exception
+				raise Exception(f'Could not find INSTALLED_APPS in {settings_file}')
+			
+			installed_apps = codeblock.group(1)
+			if not installed_apps:
+				# TODO better exception
+				raise Exception(f'No apps found in INSTALLED_APPS. Settings file: {settings_file}')
+			
+			installed_apps = installed_apps.strip()
+
+			# Check if the app is already in INSTALLED_APPS
+			if re.search(rf"'{app_name}'", installed_apps):
+				logger.info(f'{app_name} is already in INSTALLED_APPS')
+				return True
+			
+			# Modify contents with regex to include the app at the end of the list
+			contents = re.sub(r'INSTALLED_APPS = \[(.*)\]', rf'INSTALLED_APPS = [\1, \'{app_name}\']', contents, re.DOTALL)
+		
+		# Write the new contents
+		with open(settings_file, 'w') as f:
+			f.write(contents)
+
+		return True
+	
+	def append_django_middleware(self, middleware_name: str) -> bool:
+		"""
+		Add the middleware to the MIDDLEWARE list in the settings/base.py file
+		"""
+		settings_file = f'{self.backend_dir}/{self.project_name}/settings/base.py'
+
+		# Load the entire settings/base.py file into memory
+		# TODO try catch
+		with open(settings_file, 'r') as f:
+			contents = f.read()
+
+			# Get the installed apps list
+			codeblock = re.search(r'MIDDLEWARE = \[(.*)\]', contents, re.DOTALL)
+			if not codeblock:
+				# TODO better exception
+				raise Exception(f'Could not find MIDDLEWARE in {settings_file}')
+			
+			middleware = codeblock.group(1)
+			if not middleware:
+				# TODO better exception
+				raise Exception(f'No middleware found in MIDDLEWARE. Settings file: {settings_file}')
+			
+			middleware = middleware.strip()
+
+			# Check if the middleware is already in MIDDLEWARE
+			if re.search(rf"'{middleware_name}'", middleware):
+				logger.info(f'{middleware_name} is already in MIDDLEWARE')
+				return True
+			
+			# Modify contents with regex to include the middleware at the end of the list
+			contents = re.sub(r'MIDDLEWARE = \[(.*)\]', rf'MIDDLEWARE = [\1, \'{middleware_name}\']', contents, re.DOTALL)
+		
+		# Write the new contents
+		with open(settings_file, 'w') as f:
+			f.write(contents)
+
+		return True
+	
+	def nuxt_setup(self) -> str:
+		"""
+		Setup the Nuxt project and app with given names.
+
+		Returns:
+			A string indicating the status of the setup.
+		"""
+		self.setup_frontend()
+
+		# Handle dependencies for nuxt
+		self.run_subprocess(["npm", "install", "-y", "@nuxtjs/auth", "create-nuxt-app"])
+		self.pip_install('django-rest-framework dj-rest-auth django-allauth django-cors-headers djangorestframework-simplejwt')
+
+		# Setup the Nuxt project
+		self.run_subprocess(["npx", "create-nuxt-app", self.frontend_dir])
+
+		# Append necessary django apps to the settings file
+		for app_name in ['corsheaders', 'rest_framework']:
+			self.append_django_apps(app_name)
+
+		# Append middleware to the settings file
+		for middleware_name in ['corsheaders.middleware.CorsMiddleware']:
+			self.append_django_middleware(middleware_name)
+
+		"""
+		Remaining tasks to do:
+		* Add the following lines to enable JWT Authentication:
+		REST_FRAMEWORK = {
+			'DEFAULT_AUTHENTICATION_CLASSES': (
+				'rest_framework_simplejwt.authentication.JWTAuthentication',
+			),
+		}
+	
+		* Configure Django URLs
+			urlpatterns = [
+				path('admin/', admin.site.urls),
+				path('api/v1/', include('dj_rest_auth.urls')),
+			]
+
+		* Add the following lines to nuxt.config.js
+			modules: [
+				'@nuxtjs/auth',
+			],
+			auth: {
+				strategies: {
+					local: {
+						endpoints: {
+							login: { url: '/api/v1/login/', method: 'post', propertyName: 'token' },
+							logout: { url: '/api/v1/logout/', method: 'post' },
+							user: { url: '/api/v1/user/', method: 'get', propertyName: 'user' }
+						},
+						tokenRequired: true,
+						tokenType: 'Bearer',
+					}
+				}
+			}
+
+		* For new nuxtjs components:
+			this.$auth.loginWith('local', {
+				data: {
+					username: this.username,
+					password: this.password
+				}
+			})
+		"""
+		return f"Nuxt setup completed for {self.project_name}"
 
 	def install_dependencies(self) -> None:
 		"""
@@ -173,11 +443,12 @@ class App:
 		# If ubuntu, install with apt
 		if os_name == 'Linux' and shutil.which('apt'):
 			try:
-				self.run_subprocess(['apt', 'install', 'npm'])
+				self.run_subprocess(['apt', 'install', 'npm', 'npx'])
 				return "npm installed successfully on Ubuntu"
 			except subprocess.CalledProcessError as process_e:
 				raise EnvironmentError(f"Error installing npm on Ubuntu: {process_e}") from process_e
 		
+		# Otherwise, check for other variants of linux
 		if os_name in ['Linux', 'Darwin']:
 			try:
 				self.run_subprocess(['curl', 'https://www.npmjs.com/install.sh', '|', 'sh'])
@@ -266,9 +537,10 @@ class App:
 		os.makedirs(self.directory, exist_ok=True)
 
 		self.check_environment()
+		self.create_venv()
 		self.install_dependencies()
 		self.django_setup()
-		self.angular_setup()
+		self.nuxt_setup()
 		self.confirm_setup()
 
 	def run(self, command : Actions, callback : Optional[Callable] = None, *args, **kwargs) -> str:
@@ -398,7 +670,7 @@ class App:
 		"""
 		server_process_names = ["runserver", "gunicorn", "daphne"]
 		for process in psutil.process_iter(['name']):
-			if process.info['name'] in server_process_names:
+			if process.name() in server_process_names:
 				return True
 
 		return False
@@ -456,7 +728,7 @@ class App:
 				logger.debug('Django started successfully')
 				self.on_django_started()
 
-	def perform(self, command: Actions, page_name: str = None, model_name: str = None) -> Any:
+	def perform(self, command: Actions, *args, **kwargs) -> Any:
 		"""
 		Perform an action given a (string) command
 
@@ -488,14 +760,22 @@ class App:
 			case Actions.STATUS:
 				# Determine the status of our app
 				return self.status
+			case Actions.INSTALL:
+				# Install a python package using pip, and add it (with version) to requirements.txt.
+				package_name = self.get_argument('package_name', args, kwargs)
+				return self.pip_install(package_name)
 			case Actions.PAGE:
+				# Ensure we have either one *arg, or a page_name **kwarg
+				page_name = self.get_argument('page_name', args, kwargs)
+					
 				if page_name:
 					return self.create_new_page(page_name)
 				else:
 					raise ValueError("Page name is required for 'page' action.")
 			case Actions.MODEL:
+				model_name : str = self.get_argument('model_name', args, kwargs)
 				if model_name:
-					return self.create_django_model(model_name, model_name.capitalize())
+					return self.create_new_model(model_name)
 				else:
 					raise ValueError("Model name is required for 'model' action.")
 			case _:
@@ -631,36 +911,31 @@ class App:
 			with open(output_file, "w") as file:
 				file.write(rendered_template)
 
-
 def main():
-	"""
-	This code is only run when this script is called directly (i.e. python bin/app.py)
-	"""
-	parser = argparse.ArgumentParser(description='Setup and manage the Django application.')
-	parser.add_argument('action', choices=[e.value for e in Actions], help='The action to perform.')
-	parser.add_argument('-p', '--project-name', default='myproject', help='The name of the project.')
-	parser.add_argument('-a', '--author-name', help='The name of the author.')
-	parser.add_argument('-d', '--directory', default='.', help='The directory for the project.')
-	parser.add_argument('-f', '--frontend-dir', default='frontend', help='The directory for the frontend (relative to -d).')
-	parser.add_argument('-b', '--backend-dir', default='backend', help='The directory for the backend (relative to -d).')
-	parser.add_argument('-s', '--settings', default='conf/settings.yaml', help='The settings file to use.')
-	parser.add_argument('--page-name', help='The name of the page to create.')
-	parser.add_argument('--model-name', help='The name of the model to create.')
-	args = parser.parse_args()
-
-	# Load settings
-	settings = Settings(args.settings)
-
-	app = App(args.project_name, args.author_name, settings, args.directory, args.frontend_dir, args.backend_dir)
-	command = Actions(args.action)
-
-	result = app.perform(command, args.page_name, args.model_name)
-	if result is not None:
-		print(f'App returned ({result})')
-
-if __name__ == '__main__':
 	try:
-		main()
+		parser = argparse.ArgumentParser(description='Setup and manage the Django application.')
+		parser.add_argument('action', choices=[e.value for e in Actions], help='The action to perform.')
+		parser.add_argument('-p', '--project-name', default='myproject', help='The name of the project.')
+		parser.add_argument('-a', '--author-name', help='The name of the author.')
+		parser.add_argument('-d', '--directory', default='.', help='The directory for the project.')
+		parser.add_argument('-f', '--frontend-dir', default='frontend', help='The directory for the frontend (relative to -d).')
+		parser.add_argument('-b', '--backend-dir', default='backend', help='The directory for the backend (relative to -d).')
+		parser.add_argument('-s', '--settings', default='conf/settings.yaml', help='The settings file to use.')
+		parser.add_argument('--page-name', help='The name of the page to create.')
+		parser.add_argument('--model-name', help='The name of the model to create.')
+		parser.add_argument('--package-name', help='The name of the package to create.')
+		args = parser.parse_args()
+
+		# Load settings
+		settings = Settings(args.settings)
+
+		app = App(args.project_name, args.author_name, settings, args.directory, args.frontend_dir, args.backend_dir)
+		command = Actions(args.action)
+
+		result = app.perform(command, page_name=args.page_name, model_name=args.model_name, package_name=args.package_name)
+		if result is not None:
+			print(f'App returned ({result})')
+
 	except KeyboardInterrupt:
 		logger.info('Shutting down server...')
 		sys.exit(0)
@@ -673,3 +948,9 @@ if __name__ == '__main__':
 	except Exception as e:
 		print(f"Error: {e}")
 		sys.exit(0)
+
+if __name__ == '__main__':
+	"""
+	This code is only run when this script is called directly (i.e. python bin/app.py)
+	"""
+	main()
